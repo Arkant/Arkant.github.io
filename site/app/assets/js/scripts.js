30c3a6bcf3267d5a08abaf125c7416a8e2e27357
Arkant <pan_rostislav@ukr.net>
2018-12-03 11:11:35 +0200

    site

---
+++ site/app/assets/js/scripts.js
@@ -0,0 +1,23 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2018. MIT licensed.
+ */
+(function($){
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this).closest('.menu')
+        .toggleClass('menu_state_open');
+    });
+    
+    $('.menu__links-item').on('click', function() {
+      // do something
+
+      $(this).closest('.menu')
+        .removeClass('menu_state_open');
+    });
+  });
+})(jQuery);
